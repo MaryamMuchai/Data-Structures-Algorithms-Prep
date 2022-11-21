4f39280566fde89a3892ee8f2de75965a8009ceb
Maryam Muchai <maryammuchai@gmail.com>
2022-11-21 13:19:10 +0300

    linking node with references

---
+++ src/linked_list/linked.js
@@ -0,0 +1,11 @@
+// linked list Data structure in Javascript
+//linking with references
+// const n1 = {
+//     data: 100
+// };
+// const n2 = {
+//     data: 700
+// };
+// n1.next = n2;// adding reference from n1 to n2
+// console.log(n1)
+
